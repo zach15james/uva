fb0d81639a2e29617b4c8dea78d0b5879b9a9290
Zach James <zach15james@proton.me>
2025-09-22 18:36:36 -0500

    12372

--- 12372.cpp
+++ 12372.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 int main()
 {
+    int t;
+    scanf("%d", &t);
+    vector<bool> results(t);
 
+    for(int i = 0; i < t; i++)
+    {
+        int L, W, H;
+        scanf("%d %d %d", &L, &W, &H);
+        results[i] = (L <= 20 && W <= 20 && H <= 20); // boolean expression - hope it works 
+    }
 
-
+    for(int i = 0; i < t; i++)
+    {
+        printf("Case %d: %s\n", i + 1, ((results[i]) ? "good" : "bad"));
+    }
 
     return 0;
 }
